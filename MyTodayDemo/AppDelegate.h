45924a9244860843c19b3aebeead31e2041c8d3f
刘锦 <522978593@qq.com>
2014-07-23 10:20:47 +0800

    Initial Commit

---
+++ MyTodayDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyTodayDemo
+//
+//  Created by liujin on 14-7-23.
+//  Copyright (c) 2014年 liujin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyTodayDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyTodayDemo
+//
+//  Created by liujin on 14-7-23.
+//  Copyright (c) 2014年 liujin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
